c04a930fc3d603ae9042720be1bd20e497a4a085
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-08-11 18:06:30 +0530

    Fourth commit

--- Test.java
+++ Test.java
@@ -5,6 +5,12 @@ public static void main(String args[])
 {
 int String=10;
 System.out.println(String);
+int exit=20;
+System.out.println(exit);
+byte b=137;
+System.out.println(b);
+//compile time error due to incomptable types as compiler perform type checking it will check whether data type can hold literal or not by default compiler keep literal of type numeric as  int .
+
 }
 
 }
