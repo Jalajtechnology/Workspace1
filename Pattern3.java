5297060616b6fdb771d5c93741f0ddacf1b5b10f
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-08-19 19:26:46 +0530

    fifth commit

---
+++ Pattern3.java
@@ -0,0 +1,15 @@
+//*
+//*
+//*
+//*
+//*
+import java.util.*;
+public class Pattern3
+{
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++)
+		{
+		    System.out.println("*");
+		}
+	}
+}
