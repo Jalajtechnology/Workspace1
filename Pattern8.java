ee51584778c087c238a929ceb92b3d9aaaaa8d83
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-08-23 19:35:47 +0530

    Sixth Commit

---
+++ Pattern8.java
@@ -0,0 +1,31 @@
+    *
+   ***
+  *****
+ *******
+*********
+
+
+
+
+
+import java.util.*;
+public class Pattern8
+{
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+		    for(int j=0;j<n-i;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    for(int j=0;j<(2*i-1);j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
