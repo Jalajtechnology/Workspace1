63c2939ff294f116320dc21dd7b141cfc34d9bab
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-08-23 19:37:19 +0530

    Sixth Commit

---
+++ Pattern11.java
@@ -0,0 +1,60 @@
+
+
+************
+*****  *****
+****    ****
+***      ***
+**        **
+*          *
+*          *
+**        **
+***      ***
+****    ****
+*****  *****
+************
+
+
+import java.util.*;
+public class Pattern11
+{
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    for(int j=0;j<n-i;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    for(int j=0;j<(2*i);j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    for(int j=0;j<n-i;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		        
+		}
+		for(int i=n-1 ;i>=0;i--)
+		{
+		    for(int j=0;j<n-i;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    
+		    for(int j=0;j<2*i;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    for(int j=0;j<n-i;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+		
+}
