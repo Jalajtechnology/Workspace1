83474ec55fd8557c864436898f5794442dab06bf
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-09-16 22:10:08 +0530

    Committed

---
+++ PrintingString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class PrintingString
+{
+	public static void main(String[] args) {
+		String str="jalajagarwal";
+		for(int i=0;i<str.length();i++)
+		{
+		    System.out.print(str.charAt(i));
+		}
+		
+	}
+}
