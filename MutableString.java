192b83903e4d876f86b51785f627332e1f1bc8b5
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-09-19 00:15:12 +0530

    Committed

---
+++ MutableString.java
@@ -0,0 +1,9 @@
+import java.lang.String;
+public class MutableString 
+{
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("jalaj");
+		s.append("agarwal");
+		System.out.println(s);
+	}
+}
