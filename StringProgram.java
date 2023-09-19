e5d028edbe3a3832c258a12b25c7852e839c3d90
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-09-19 21:04:44 +0530

    Committed

---
+++ StringProgram.java
@@ -0,0 +1,8 @@
+import java.lang.String;
+public class StringProgram
+{
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("jalajagarweal");
+		System.out.println(s.charAt(4));
+	}
+}
