b4e9d17cfa2a42d6ce95bbf150487ecbc4178872
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-09-22 00:17:16 +0530

    committed

---
+++ SimpleProgram.java
@@ -0,0 +1,20 @@
+import java.lang.String;
+public class SimpleProgram
+{
+	public static void main(String[] args) {
+		int []arr={10,20,30};
+		display(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+		    System.out.println(arr[i]);
+		}
+	}
+	public static void display(int[]brr)
+	{
+	    brr[0]=50;
+	    for(int i=0;i<brr.length;i++)
+	    {
+	        System.out.println(brr[i]);
+	    }
+	}
+}
