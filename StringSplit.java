3accb5d7085aa5e4025905771feea23c95319649
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-09-23 18:57:23 +0530

    committed

---
+++ StringSplit.java
@@ -0,0 +1,15 @@
+import java.lang.String;
+public class StringSplit
+{
+	public static void main(String[] args) {
+		String str1="jalaj agarwal";
+		String arr[]=str1.split(" ");
+		for(int i=0;i<arr.length;i++)
+		{
+		    System.out.println(arr[i]);
+		}
+		
+		
+		
+	}
+}
