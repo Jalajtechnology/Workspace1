5603eb9d866d3d90cfb71a36a260188147c3fffa
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-09-26 00:07:46 +0530

    FinalImmutableRefrenceVariable.java

---
+++ FinalImmutableRefrenceVariable.java
@@ -0,0 +1,12 @@
+public class FinalImmutableRefrenceVariable
+{
+	public static void main(String[] args) {
+	    final String s1="jalajagarwal";
+	    String s2="jalaj"+"agarwal";
+	    System.out.println(s1==s2);
+		
+		
+		
+		
+	}
+}
